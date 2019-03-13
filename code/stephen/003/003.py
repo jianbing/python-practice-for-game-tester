861431d816b68563d2040d86f70eccdb3886fc51
Stephen0910 <281016636@qq.com>
2019-03-13 14:16:10 +0800

    stephen (#4)
    * 001 002 003

---
+++ code/stephen/003/003.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Time     : 2019/3/13 0013 下午 1:17
+# @Author   : Stephen
+# @Site     : 
+# @File     : 003.py
+# @Purpose  :
+# @Software : PyCharm
+# @Copyright:   (c) Stephen 2019
+# @Licence  :     <your licence>
+import os
+
+catalog = ""  # 设置路径，如果不设置则为当前路径
+
+
+class Folder():
+    def __init__(self):
+        if catalog == "":
+            self.catalog = os.getcwd()
+        else:
+            self.catalog = catalog
+        print self.catalog
+        self.apk_list = os.listdir(self.catalog)
+
+    def install(self):
+        for i in self.apk_list:
+            if str(i)[-4:0] == ".apk":
+                os.system("adb install {0}".format(i))
+
+z = Folder()
+z.install()
